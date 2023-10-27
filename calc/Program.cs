499861402c3d93c6f7246965caef217d4307e5ee
voroz <voroz@VAREOMLLL>
2023-10-27 20:39:22 +0600

    Добавьте файлы проекта.

---
+++ calc/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double n = 0;
+
+            string c = Console.ReadLine();
+
+            switch (c)
+            {
+                //я второй раз работаю в VS после PyCharm и я уже обожаю VS он же просто дописывает код за мной
+                case "+":
+                    n = a + b; 
+                break;
+
+                case "-":
+                    n = a - b;
+                break;
+
+                case "*":
+                n = a * b;
+                break;
+
+                case "/":
+                n = a / b;
+                break;
+
+                case "%":   
+                n = a % b;
+                break;
+            }
+            Console.WriteLine(n);
+            Console.ReadLine();
+        }
+    }
+}
